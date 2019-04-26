cc1c104751160430428a55452f5624b9e094cace
Shivam Arora <shivam.arora@block8.io>
2019-04-26 15:53:45 +0530

    Deal with database and collections

--- lib/src/meteor/meteor.dart
+++ lib/src/meteor/meteor.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:ddp/ddp.dart';
+import 'package:mongo_dart/mongo_dart.dart';
 import '../listeners/listeners.dart';
 
 enum ConnectionStatus { CONNECTED, DISCONNECTED }
@@ -13,12 +14,13 @@ class Meteor {
   static bool isConnected = false;
   static String _currentUserId;
   static String get currentUserId => _currentUserId;
+  static Db db = null;
 
   static Future<ConnectionStatus> connect(String url) async {
     Completer<ConnectionStatus> completer = Completer<ConnectionStatus>();
 
     _connectionUrl = url;
-    _client = DdpClient("src.meteor", _connectionUrl, "src.meteor");
+    _client = DdpClient("meteor", _connectionUrl, "meteor");
     _client.connect();
 
     _client.addStatusListener((status) {
@@ -64,43 +66,50 @@ class Meteor {
 /**
  * Methods associated with authentication
  */
-  static bool isLoggedIn(){
-    return _currentUserId!=null;
+  static bool isLoggedIn() {
+    return _currentUserId != null;
   }
 
-  static void loginWithPassword(String email,String password, ResultListener resultListener) async {
-    if(isConnected){
-      var result = await _client.call("login", [{"password":password,"user":{"email":email}}]);
+  static void loginWithPassword(
+      String email, String password, ResultListener resultListener) async {
+    if (isConnected) {
+      var result = await _client.call("login", [
+        {
+          "password": password,
+          "user": {"email": email}
+        }
+      ]);
       print(result.reply);
       notifyLoginResult(result, resultListener);
     }
   }
 
-  static void loginWithToken(String token, ResultListener resultListener) async{
-    if(isConnected){
-      var result = await _client.call("login", [{"resume":token}]);
+  static void loginWithToken(
+      String token, ResultListener resultListener) async {
+    if (isConnected) {
+      var result = await _client.call("login", [
+        {"resume": token}
+      ]);
       print(result.reply);
       notifyLoginResult(result, resultListener);
     }
-
   }
 
   static void notifyLoginResult(Call result, ResultListener resultListener) {
     String userId = result.reply["id"];
     String token = result.reply["token"];
-    if(userId!=null){
+    if (userId != null) {
       _currentUserId = userId;
-      if(resultListener!=null) {
+      if (resultListener != null) {
         resultListener.onSuccess(token);
       }
-    }
-    else{
+    } else {
       _notifyError(resultListener, result);
     }
   }
 
-  static void logout() async{
-    if(isConnected){
+  static void logout() async {
+    if (isConnected) {
       var result = await _client.call("logout", []);
       print(result.reply);
     }
@@ -108,5 +117,36 @@ class Meteor {
 
   static void _notifyError(ResultListener resultListener, Call result) {
     resultListener.onError(result.reply['reason']);
+  }
+
+  /**
+   * Methods associated with connection to MongoDB
+   */
+  static Future<Db> getMeteorDatabase() async {
+    Completer<Db> completer = Completer<Db>();
+    if (db == null) {
+      final uri = Uri.parse(_connectionUrl);
+      String dbUrl = "mongodb://" + uri.host + ":3001/meteor";
+      print("Connecting to $dbUrl");
+      db = Db(dbUrl);
+      await db.open();
+    }
+    completer.complete(db);
+    return completer.future;
+  }
+
+  static Db getCustomDatabase(String dbUrl) {
+    return Db(dbUrl);
+  }
+
+/**
+ * Methods associated with current user
+ */
+  static Future<Map<String, dynamic>> userAsMap() async {
+    Completer completer = Completer<Map<String, dynamic>>();
+    Db db = await getMeteorDatabase();
+    var user = await db.collection("users").findOne({"_id": _currentUserId});
+    completer.complete(user);
+    return completer.future;
   }
 }
